f5bbc56d51a6049540e5cd92ee24f7f3ed827889
Tim <tim8912097887@gmail.com>
2026-04-02 08:28:37 +0800

    Configure tsconfig and eslint for central_bank service

---
+++ central_bank/eslint.config.mjs
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: [
+      "js/recommended",
+      ...tseslint.configs.recommended
+    ], 
+    languageOptions: { 
+      globals: globals.node,
+      parserOptions: {
+        project: true, // Required for type-checked rules
+        tsconfigRootDir: import.meta.dirname,
+      }
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
+      "@typescript-eslint/no-explicit-any": [
+        "off"
+      ],
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "no-console": "error",
+      "eqeqeq": ["error", "always"],
+    } 
+  },
+]);
